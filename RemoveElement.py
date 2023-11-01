3f4e25978cb6eba5bed12fde259861a8b54cd4fb
MADHUVEER <111329658+KoilakondaMadhu@users.noreply.github.com>
2023-11-01 13:32:51 +0530

    Update RemoveElement.py

--- RemoveElement.py
+++ RemoveElement.py
@@ -17,7 +17,7 @@
 # It does not matter what you leave beyond the returned k (hence they are underscores).
 
 
-# class Solution:
+class Solution:
     def removeElement(self, nums, val):
         index = 0
         for i in range(len(nums)):
